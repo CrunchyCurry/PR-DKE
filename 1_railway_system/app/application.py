3a4a88659187216ab4249738799f911a84114ef7
CrunchyCurry <philip.duong@hotmail.com>
2021-12-17 00:24:27 -0800

    #9: Create CRUD operations for stations

--- 1_railway_system/app/application.py
+++ 1_railway_system/app/application.py
@@ -1,8 +1,11 @@
+from functools import wraps
+
 import flask_bcrypt
-from flask import Flask, render_template, url_for, flash, redirect, request, jsonify
+from flask import Flask, render_template, url_for, flash, redirect, request, jsonify, session
 from flask_sqlalchemy import SQLAlchemy
-import forms  #import RegisterForm, LoginForm, StationForm
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
+import forms  # import RegisterForm, LoginForm, StationForm # TODO: packaging
 import os
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
@@ -17,6 +20,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # "sqlite:1_railway_system/resources/railway.db"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
 
 # Init ma
 ma = Marshmallow(app)
@@ -24,7 +30,13 @@ ma = Marshmallow(app)
 
 # migrate = Migrate(app, db)
 
-class User(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True)
@@ -40,12 +52,6 @@ class Railway(db.Model):
     __tablename__ = "railways"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=True)
-    # starts_at = db.Column(db.Integer, db.ForeignKey("station.id"), nullable=False)
-    # ends_at = db.Column(db.Integer, db.ForeignKey("station.id"), nullable=False)
-
-    # starts_at = db.Column(db.Integer, db.ForeignKey("station.id"))
-    # ends_at = db.Column(db.Integer, db.ForeignKey("station.id"))
-
     starts_at = db.Column(db.Integer, db.ForeignKey("stations.id"))
     ends_at = db.Column(db.Integer, db.ForeignKey("stations.id"))
 
@@ -87,22 +93,20 @@ stations_schema = StationSchema(many=True)  # for multiple returns values
 #     max_speed = db.Column(db.Integer)
 #     gauge = db.Column(db.Integer)
 
+# -----------------------------------------------------
 
 @app.route("/")
+@login_required
 def home():
     railways = Railway.query.all()
     return render_template("home.html", railways=railways)
 
 
-@app.route("/stations")
-def stations():
-    stations = Station.query.all()
-    return render_template("stations.html", stations=stations)
-
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    form = RegisterForm()
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = forms.RegisterForm()
     if form.validate_on_submit():
         hashed_password = flask_bcrypt.generate_password_hash(form.password.data).decode("utf-8")
         user = User(username=form.username.data, password=hashed_password, type=True)
@@ -115,20 +119,44 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    form = LoginForm()
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = forms.LoginForm()
     if form.validate_on_submit():
-        if form.username.data == "admin" and form.password.data == "admin":
-            flash("Erfolgreich angemeldet!", "success")
-            return redirect(url_for("home"))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)  # log user in
+            next_page = request.args.get("next")  # get last page that was accessed to redirect user back to it
+            return redirect(next_page) if next_page else redirect(url_for("home"))
         else:
-            flash("Anmeldung fehlgeschlagen. Benutzername und/oder Passwort falsch!", "danger")
+            flash("Anmeldung fehlgeschlagen. Bitte Benutzername und Passwort prüfen.", "danger")
     return render_template("login.html", title="Anmelden", form=form)
 
 
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template("account.html", title="Account")
+
+
+@app.route("/stations")
+@login_required
+def stations():
+    stations = Station.query.all()
+    return render_template("stations.html", title="Bahnhöfe", stations=stations)
+
+
 @app.route("/station/new", methods=["GET", "POST"])
+@login_required
 # @login_required
 def new_station():
-    form = StationForm()
+    form = forms.StationForm()
     if form.validate_on_submit():
         station = Station(name=form.name.data, state=form.state.data)
         db.session.add(station)
@@ -140,16 +168,18 @@ def new_station():
 
 
 @app.route("/station/<int:station_id>")
+@login_required
 def station(station_id):
     station = Station.query.get_or_404(station_id)
     return render_template("station.html", title=station.name, station=station)
 
 
 @app.route("/station/<int:station_id>/update", methods=["GET", "POST"])
+@login_required
 # @login_required
 def update_station(station_id):
     station = Station.query.get_or_404(station_id)
-    form = StationForm()
+    form = forms.StationForm()
     # fill in previous data
     if form.validate_on_submit():
         station.name = form.name.data
@@ -166,8 +196,8 @@ def update_station(station_id):
 
 #  <!-- check whether user is admin {% if current_user == admin %} --> <!-- endif check -->
 
-
 @app.route("/station/<int:station_id>/delete", methods=["POST"])
+@login_required
 # @login_required
 def delete_station(station_id):
     station = Station.query.get_or_404(station_id)
@@ -192,5 +222,17 @@ def api_station(id):
     return station_schema.jsonify(station)
 
 
+# -----------------------------------------------------
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get('username') is None or session.get('if_logged') is None:
+            return redirect(url_for("login"), code=302)
+        return f(*args, **kwargs)
+
+    return decorated_function
